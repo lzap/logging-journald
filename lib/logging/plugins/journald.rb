c48ef446e0f357b3cf835d0ad8a6488c45a60330
Lukas Zapletal <lzap+git@redhat.com>
2018-10-15 09:45:06 +0200

    Version 2.0.2

--- lib/logging/plugins/journald.rb
+++ lib/logging/plugins/journald.rb
@@ -3,7 +3,7 @@ module Logging
     module Journald
       extend self
 
-      VERSION ||= '2.0.1'.freeze
+      VERSION ||= '2.0.2'.freeze
 
       def initialize_journald
         require File.expand_path('../../layouts/noop', __FILE__)
